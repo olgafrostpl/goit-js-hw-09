0c317854869efde3bfd8877743070c8414e96578
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-06-23 21:52:55 +0300

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let timerID;
+
+startButton.addEventListener('click', () => {
+  timerID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerID);
+
+  startButton.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
